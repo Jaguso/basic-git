f85bd55b2a380c5e4249231fe7dfe16a70187b8d
Jaguso <jagutzo@gmail.com>
2023-07-04 17:14:42 -0600

    Añadí filtrador de pares

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('hola')
+lista = [1,2,3,4]
+
+def filtrar_pares(list):
+    final = []
+    for item in list:
+        if item%2==0:
+            final.append(item)
+    return final
+            
